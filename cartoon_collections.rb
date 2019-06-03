21294bf8c25da5687be20de05fe376b4c3a0300a
Chris Lincoln <chris.lincoln12@gmail.com>
2019-06-03 20:20:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,6 +16,8 @@ def long_planeteer_calls(calls)
 end
 
 def find_the_cheese(array)
-  # the array below is here to help
+
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  array.include?(cheese_types)
 end
